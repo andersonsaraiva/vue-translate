eb376289ddb331523bfb95ca5fc0bde45b9b92e5
Anderson Saraiva Lima <anderson.lima@i4pro.com.br>
2019-07-02 11:21:28 -0300

    criando o respositorio com uma poc de tradução do app vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <button
+        v-for="entry in languages"
+        :key="entry.title"
+        @click="changeLocale(entry.language)"
+      >{{entry.title}}</button>
+    </div>
+
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import i18n from "@/plugins/i18n";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      languages: [
+        { flag: "us", language: "en", title: "English" },
+        { flag: "es", language: "es", title: "Español" }
+      ]
+    };
+  },
+  methods: {
+    changeLocale(locale) {
+      i18n.locale = locale;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  padding: 15px;
+  border: 1px solid green;
+  font-size: 18px;
+  margin: 15px;
 }
 </style>
